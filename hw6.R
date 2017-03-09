1578f51d68bc86a5f4fde14d9020ad550ededd6a
Walter Reilly <wbreilly@ucdavis.edu>
2017-03-08 23:09:31 -0800

    progress on hw 6

--- hw6.R
+++ hw6.R
@@ -5,7 +5,7 @@ library(tidyverse)
 
 # setup df
 d = read.table("aids.txt", quote="\"", comment.char="")
-colnames(d) = c("ID","Treatment","Age","Gender(Male=1)","Week","logCD4")
+colnames(d) = c("ID","Treatment","Age","Gender","Week","logCD4")
 
 # get random IDs
 rand.samp = sample.int(1313, size = 50, replace = FALSE)
@@ -15,3 +15,10 @@ dspag = d %>% filter(ID %in% rand.samp)
 #spag plot
 p <- ggplot(data = dspag, aes(x = Week, y = logCD4, group = ID, colour = factor(Treatment),guide_legend(title = "Treatment")))
 p  + geom_line() + ggtitle("AIDS") 
+
+######################
+#2
+library(nlme)
+
+m1 = lme(logCD4 ~ Week + Treatment + Gender , random = ~ 1  | ID, method = "REML", data = d) #random intercept only
+summary(m1)
