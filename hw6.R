93bbd1591fd5ac78c97b00706ec46d76d2a88f3f
Walter Reilly <wbreilly@ucdavis.edu>
2017-03-09 10:58:39 -0800

    good prog

--- hw6.R
+++ hw6.R
@@ -13,12 +13,19 @@ rand.samp = sample.int(1313, size = 50, replace = FALSE)
 dspag = d %>% filter(ID %in% rand.samp)
 
 #spag plot
-p <- ggplot(data = dspag, aes(x = Week, y = logCD4, group = ID, colour = factor(Treatment),guide_legend(title = "Treatment")))
-p  + geom_line() + ggtitle("AIDS") 
+p <- ggplot(data = dspag, aes(x = Week, y = logCD4, group = ID, colour = factor(Treatment)))
+p  + geom_line() + ggtitle("AIDS") + labs(colour = "Treatment")
 
 ######################
 #2
 library(nlme)
 
+# mixed effects mod with random intercept for ID
+d$Treatment = as.factor(d$Treatment)
 m1 = lme(logCD4 ~ Week + Treatment + Gender , random = ~ 1  | ID, method = "REML", data = d) #random intercept only
 summary(m1)
+
+#3
+# miced effects model with random int and random slope for week
+m2 = lme(logCD4 ~ Treatment + Gender , random = ~ 1 + Week | ID, method = "REML", data = d) #random intercept only
+summary(m2)
